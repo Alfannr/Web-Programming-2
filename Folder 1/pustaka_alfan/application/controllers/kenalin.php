8092ec817ca416a19d75ea35845dde70ff8ddb5c
Alfannr <alfann812@gmail.com>
2020-10-31 14:05:38 +0700

    first commit

---
+++ Folder 1/pustaka_alfan/application/controllers/kenalin.php
@@ -0,0 +1,11 @@
+<?php
+class kenalkan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalan</h1>";
+        echo "Alfan Nur Rabbani<br>
+                saya tinggal di Nakula <br>
+                saya suka olahraga";
+    }
+}
